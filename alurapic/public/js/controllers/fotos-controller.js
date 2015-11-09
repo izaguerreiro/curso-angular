2dc063014780feafbac617970806e583ccbf0efb
izaguerreiro <izaguerreiro@gmail.com>
2015-11-09 16:09:20 -0200

    Atividades do Capítulo 2. Utilizando get para exibir uma lista de registros

--- alurapic/public/js/controllers/fotos-controller.js
+++ alurapic/public/js/controllers/fotos-controller.js
@@ -1,6 +1,20 @@
-angular.module('alurapic').controller('FotosController', function($scope){
-	$scope.foto = {
-		titulo: "porquinho",
-		url: "http://www.livredeparasitas.com/static/media/images/upload/porquinho.jpg"
-	}
+angular.module('alurapic').controller('FotosController', function($scope, $http){
+	$scope.fotos = [];
+
+	$http.get('v1/fotos')
+	.success(function(fotos){
+		$scope.fotos = fotos;
+	})
+	.error(function(erro){
+		console.log(erro)
+	});
+
+	/*
+	var promise = $http.get('v1/fotos');
+	promise.then(function(retorno){
+		$scope.fotos = retorno.data;
+	}).catch(function(error){
+		console.log(error);
+	});
+	*/
 });
