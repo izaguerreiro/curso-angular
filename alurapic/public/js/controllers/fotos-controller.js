145c7bb5c97c250ff4373df553a9c1999e897243
Izabela Guerreiro <izaguerreiro@gmail.com>
2015-11-08 23:56:45 -0200

    Atividades do capítulo 1. Criação do Criação do módulo alurapic.

---
+++ alurapic/public/js/controllers/fotos-controller.js
@@ -0,0 +1,6 @@
+angular.module('alurapic').controller('FotosController', function($scope){
+	$scope.foto = {
+		titulo: "porquinho",
+		url: "http://www.livredeparasitas.com/static/media/images/upload/porquinho.jpg"
+	}
+});
