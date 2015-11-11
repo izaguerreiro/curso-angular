82c088531d7dfd9be72be67978d3a80ce3649f6a
izaguerreiro <izaguerreiro@gmail.com>
2015-11-11 15:25:24 -0200

    Atividades Capitulo 5. Single Page Application

--- alurapic/public/js/main.js
+++ alurapic/public/js/main.js
@@ -1,1 +1,15 @@
-angular.module('alurapic', ['minhasDiretivas', 'ngAnimate']);
+angular.module('alurapic', ['minhasDiretivas', 'ngAnimate', 'ngRoute'])
+.config(function($routeProvider, $locationProvider){
+	$locationProvider.html5Mode(true);
+
+	$routeProvider.when('/fotos', {
+		templateUrl: 'partials/principal.html', 
+		controller: 'FotosController'
+	});
+	
+	$routeProvider.when('/fotos/new', {
+		templateUrl: 'partials/fotos.html', 
+	});
+	
+	$routeProvider.otherwise({redirectTo: '/fotos'});
+});
