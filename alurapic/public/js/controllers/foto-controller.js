da42e67ca08e53af34e34ee2d50b7b057792d5ed
Izabela Guerreiro <izaguerreiro@gmail.com>
2015-11-15 11:10:47 -0200

    Atividades Capítulo 9. Módulo ngResource

--- alurapic/public/js/controllers/foto-controller.js
+++ alurapic/public/js/controllers/foto-controller.js
@@ -1,36 +1,30 @@
-angular.module('alurapic').controller('FotoController', function($scope, $http, $routeParams){
+angular.module('alurapic').controller('FotoController', function($scope, $routeParams, recursoFoto){
 	$scope.foto = {};
 	$scope.mensagem = '';
-	
+
 	if ($routeParams.fotoId){
-		$http.get('v1/fotos/' + $routeParams.fotoId)
-		.success(function(foto){
+		recursoFoto.get({fotoId: $routeParams.fotoId}, function(foto){
 			$scope.foto = foto;
-		})
-		.error(function(erro){
+		}, function(erro){
 			console.log(erro);
 			$scope.mensagem = 'Nao foi possível obter a foto';
 		});
-	}
+	};
 
 	$scope.submeter = function(){
 		if ($scope.formulario.$valid){
 			if ($scope.foto._id){
-				$http.put('v1/fotos' + $scope.foto._id, $scope.foto)
-				.success(function(){
+				recursoFoto.update({fotoId: $scope.foto._id}, $scope.foto, function(){
 					$scope.mensagem = 'A foto ' + $scope.foto.titulo + ' foi alterada com sucesso';
-				})
-				.error(function(erro){
+				}, function(erro){
 					console.log(erro);
 					$scope.mensagem = 'Não foi possível alterar a foto ' + $scope.foto.titulo;
 				});
 			} else{
-				$http.post('v1/fotos', $scope.foto)
-				.success(function(){
+				recursoFoto.save($scape.foto, function(){
 					$scope.foto = {};
 					$scope.mensagem = 'Foto incluída com sucesso!'
-				})
-				.error(function(erro){
+				}, function(erro){
 					$scope.mensagem = 'Não foi possível incluir a foto!';
 				});
 			};
--- alurapic/public/js/controllers/fotos-controller.js
+++ alurapic/public/js/controllers/fotos-controller.js
@@ -1,35 +1,22 @@
-angular.module('alurapic').controller('FotosController', function($scope, $http){
+angular.module('alurapic').controller('FotosController', function($scope, recursoFoto){
 	$scope.fotos = [];
 	$scope.filtro = '';
 	$scope.mensagem = '';
 
-	$http.get('v1/fotos')
-	.success(function(fotos){
+	recursoFoto.query(function(fotos){
 		$scope.fotos = fotos;
-	})
-	.error(function(erro){
-		console.log(erro)
+	}, function(erro){
+		console.log(erro);
 	});
 
 	$scope.remover = function(foto){
-		$http.delete('v1/fotos/' + foto._id)
-		.success(function(){
+		recursoFoto.delete({fotoId: foto._id}, function(){
 			var indiceFoto = $scope.fotos.indexOf(foto);
 			$scope.fotos.splice(indiceFoto, 1);
 			$scope.mensagem = 'Foto ' + foto.titulo + ' foi removida com sucesso!';
-		})
-		.error(function(erro){
+		}, function(erro){
 			console.log(erro);
 			$scope.mensagem = 'Não foi possível remover a foto ' + foto.titulo;
-		});
+		})
 	};
-
-	/*
-	var promise = $http.get('v1/fotos');
-	promise.then(function(retorno){
-		$scope.fotos = retorno.data;
-	}).catch(function(error){
-		console.log(error);
-	});
-	*/
 });
